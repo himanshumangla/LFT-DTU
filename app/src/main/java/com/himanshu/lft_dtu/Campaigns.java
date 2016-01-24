a9e7d33865ba15ad5025fd3147d878ec06f0bcfb
Himanshu Mangla <him.mangla@gmail.com>
2016-01-24 15:21:37 +0530

    navigation view start

---
+++ app/src/main/java/com/himanshu/lft_dtu/Campaigns.java
@@ -0,0 +1,14 @@
+package com.himanshu.lft_dtu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Himanshu on 1/22/2016.
+ */
+public class Campaigns extends Activity {
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.campaigns);
+    }
+}
